029b708bcfaa95bcb0559204df79c26d37dfe8ac
Ekaterina <117029432+soykatie@users.noreply.github.com>
2025-12-01 12:58:57 +0300

    Update Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,7 +4,7 @@ import Loading from "../components/common/loading/Loading";
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
-const repoName = import.meta.env.VITE_REPO_NAME || "";
+const repoName = "";
 
 export const router = createBrowserRouter(
   [
@@ -25,3 +25,4 @@ export const router = createBrowserRouter(
   ],
   { basename: `/${repoName}` }
 );
+
